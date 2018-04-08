e7f38b3cc960e2cfeb992005af57e9cf2754cf37
MagdaZny <zieleznym6@gmail.com>
2018-04-08 16:40:34 +0200

    Improve controllers test

--- src/test/scala/app/controllers/LetShoutTest.scala
+++ src/test/scala/app/controllers/LetShoutTest.scala
@@ -9,18 +9,20 @@ import play.api.test._
 
 class LetShoutTest extends Test with Results {
 
-  "LetShout should return json with last two tweets" in {
+  "LetShout should return string with last tweets" in {
     val userName = randomString()
     val numberOfTwitts = randomNumber()
     val twitt1 = randomString()
     val twitt2 = randomString()
-
     val shout = mock[Shout]
     when(shout.apply(userName, numberOfTwitts)).thenReturn(Seq(twitt1, twitt2))
     val controller = new LetShout(Helpers.stubControllerComponents(), shout)
 
     val result = controller.answer(userName, numberOfTwitts).apply(FakeRequest())
 
+    status(result) should be(200)
     contentAsString(result) should be(twitt1 + " " + twitt2)
+    contentType(result) should be(Some("text/plain"))
+    charset(result) should be(Some("utf-8"))
   }
 }
